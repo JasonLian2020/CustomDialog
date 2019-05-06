1d5b7bd7c1935af6bdee358f8c1b486937ff1cdf
jason <itlanguege@163.com>
2019-05-06 15:17:28 +0800

    Update MainActivity

--- app/src/main/java/me/jason/app/customdialog/MainActivity.java
+++ app/src/main/java/me/jason/app/customdialog/MainActivity.java
@@ -3,6 +3,7 @@ package me.jason.app.customdialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Toast;
 
 import butterknife.ButterKnife;
 import butterknife.OnClick;
@@ -22,20 +23,72 @@ public class MainActivity extends AppCompatActivity {
         switch (view.getId()) {
             case R2.id.btnInput:
                 new CustomDialog.Builder(this)
-                        .title("title")
-                        .content("hahaha")
+                        .title("这是一个标题")
+                        .content("请输入你的手机")
                         .negativeText("取消")
                         .positiveText("确定")
                         .dialogType(CustomDialog.DIALOG_TYPE_INPUT)
                         .show();
                 break;
             case R2.id.btnImage:
+                new CustomDialog.Builder(this)
+                        .title("这是一个标题")
+                        .content("图片")
+                        .negativeText("取消")
+                        .positiveText("确定")
+                        .dialogType(CustomDialog.DIALOG_TYPE_IMAGE)
+                        .show();
                 break;
             case R2.id.btnMessage:
+                new CustomDialog.Builder(this)
+                        .title("这是一个标题")
+                        .content("内容内容内容")
+                        .negativeText("取消")
+                        .positiveText("确定")
+                        .dialogType(CustomDialog.DIALOG_TYPE_MESSAGE)
+                        .show();
                 break;
             case R2.id.btnSingleChoice:
+                String[] denyReasons = new String[]{"该类型课程不支持退款", "课程已开始不支持退款", "其他"};
+                new CustomDialog.Builder(this)
+                        .title("拒绝退款")
+                        .items(denyReasons)
+                        .itemsCallbackSingleChoice(0, (dialog, itemView, which, text) -> false)
+                        .negativeText("取消")
+                        .positiveText("确定")
+                        .onPositive((dialog, dialogAction) -> {
+                            Toast.makeText(this, dialog.getSingleChoiceText(), Toast.LENGTH_SHORT).show();
+                        })
+                        .needInput(true)
+                        .dialogType(CustomDialog.DIALOG_TYPE_SINGLE_CHOICE)
+                        .show();
                 break;
             case R2.id.btnMultiChoice:
+                String[] items = new String[]{"1", "2", "3", "4", "5"};
+                new CustomDialog.Builder(this)
+                        .title("多选框")
+                        .items(items)
+                        .itemsCallbackMultiChoice(new Integer[]{0, 1}, (dialog, which, text) -> false)
+                        .negativeText("取消")
+                        .positiveText("确定")
+                        .onPositive((dialog, dialogAction) -> {
+                            String[] multiChoiceTexts = dialog.getMultiChoiceTexts();
+                            if (multiChoiceTexts == null) return;
+                            String content = "";
+                            for (int i = 0; i < multiChoiceTexts.length; i++) {
+                                String multiChoiceText = multiChoiceTexts[i];
+                                if (i == 0) {
+                                    content = content + multiChoiceText;
+                                } else if (i == multiChoiceTexts.length - 1) {
+                                    content = content + multiChoiceText;
+                                } else {
+                                    content = content + "\n" + multiChoiceText;
+                                }
+                            }
+                            Toast.makeText(this, content, Toast.LENGTH_SHORT).show();
+                        })
+                        .dialogType(CustomDialog.DIALOG_TYPE_MULTI_CHOICE)
+                        .show();
                 break;
         }
     }
