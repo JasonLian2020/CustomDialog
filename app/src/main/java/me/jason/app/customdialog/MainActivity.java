0d18ffb9fe38da07edaccd268163a4c3603a6585
jason <itlanguege@163.com>
2019-05-06 10:56:32 +0800

    Update MainActivity

--- app/src/main/java/me/jason/app/customdialog/MainActivity.java
+++ app/src/main/java/me/jason/app/customdialog/MainActivity.java
@@ -1,7 +1,12 @@
 package me.jason.app.customdialog;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import me.jason.library.CustomDialog;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick({R2.id.btnInput, R2.id.btnImage, R2.id.btnMessage, R2.id.btnSingleChoice, R2.id.btnMultiChoice})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R2.id.btnInput:
+                new CustomDialog.Builder(this)
+                        .title("title")
+                        .content("hahaha")
+                        .negativeText("取消")
+                        .positiveText("确定")
+                        .dialogType(CustomDialog.DIALOG_TYPE_INPUT)
+                        .show();
+                break;
+            case R2.id.btnImage:
+                break;
+            case R2.id.btnMessage:
+                break;
+            case R2.id.btnSingleChoice:
+                break;
+            case R2.id.btnMultiChoice:
+                break;
+        }
     }
 }
